4bdc3dc5e3d0e017b6ad988d8b657c1f4270a048
Asimut <asimut007@gmail.com>
2021-04-09 21:04:42 +0300

    Oleg

--- story_content/user.js
+++ story_content/user.js
@@ -2,13 +2,21 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6N64ZTSGqUc":
+      case "5XGqJRB7cv6":
         Script1();
+        break;
+      case "63R1G8zblat":
+        Script2();
         break;
   }
 }
 
 function Script1()
+{
+  window.frames[0].frameElement.contentWindow.CreateBook();
+}
+
+function Script2()
 {
   window.frames[0].frameElement.contentWindow.CreateBook();
 }
