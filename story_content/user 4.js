32982ec1436812eecd9c64fea255aaacc0d97fd6
Asimut <asimut007@gmail.com>
2021-04-10 14:09:17 +0300

    Oleg

---
+++ story_content/user 4.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5XGqJRB7cv6":
+        Script1();
+        break;
+      case "63R1G8zblat":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.frames[0].frameElement.contentWindow.CreateBook();
+}
+
+function Script2()
+{
+  window.frames[0].frameElement.contentWindow.CreateBook();
+}
+
